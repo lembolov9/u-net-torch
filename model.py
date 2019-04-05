e39796c9d6a024d07e2c2ea35ada73b89a2c2a95
lembolov9 <Tomas080808>
2019-04-05 05:06:26 +0300

    changes

--- model.py
+++ model.py
@@ -29,8 +29,7 @@ class up(nn.Module):
     def __init__(self, in_ch, out_ch):
         super(up, self).__init__()
         self.up = nn.Sequential(
-            nn.Upsample(scale_factor=2),
-            nn.Conv2d(in_ch,out_ch,kernel_size=3,stride=1,padding=1),
+            nn.ConvTranspose2d(in_ch, out_ch, 2, stride=2),
 			nn.ReLU()
 )
 
